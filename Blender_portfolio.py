67c28cebf49c6bcd963cd73db18108e6288139e7
Akshat <99664728+AkshatStark06@users.noreply.github.com>
2024-12-14 14:42:31 +0530

    Add files via upload

---
+++ Blender_portfolio.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+st.title("Stark Website")
+st.subheader("Welcome folks to Akshat Stark's Blender Portfolio")
+
+st.markdown("# A Journey through Space:")
+
+st.markdown("### Sun: A Close Encounter:")
+st.video(r"D:\just get it done\Blender\SUN final\SUN final render0001-0200.mp4", format='mp4')
+
+st.markdown("### Earth: A view from Above:")
+st.video(r"D:\just get it done\Blender\earth\earth final0001-0100.mp4", format='mp4')
+
+st.markdown("### Moon: The Satellite")
+st.video("D:\just get it done\Blender\Moon\MOON final render0001-0200.mp4", format= 'mp4')
+
+st.markdown("### Spaceship Clip:")
+st.video(r"D:\just get it done\Blender\Jupitur\spaceship final render0001-0200.mp4", format='mp4')
+
+st.markdown("### Jupiter & its Moon:")
+st.video(r"D:\just get it done\Blender\Jupitur\Jupitur final render0001-0060.mp4", format='mp4')
+
+st.markdown("### Room Decor:")
+st.image(r"D:\just get it done\Blender\room2.0.png")
+
+st.markdown("# Let's Hit the Road:")
+st.video(r"D:\just get it done\Blender\Audi A7 final video0001-1500.mp4", format='mp4')
+
+st.markdown("# Time for some Music:")
+
+st.write("Summer maddness:")
+st.audio(r"D:\just get it done\Blender\New folder\roa-music-summer-madness(chosic.com).mp3",format='mp3')
+
+st.write("Gangsta's Paradise:")
+st.audio(r"D:\just get it done\Blender\New folder\Gangsta's-Paradise(PagalWorld).mp3", format='mp3')
+
+st.markdown("## Straight from the Factory: ")
+
+st.image(r"C:\Users\Dell\Downloads\iphone-15-pro-max-camera (1).jpg")
+
+st.markdown("## Himalayas: The Heaven on Earth")
+st.image(r"D:\just get it done\Blender\pexels-eberhard-grossgasteiger-1287145.jpg")
